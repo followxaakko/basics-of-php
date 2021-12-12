c0b9b0a709cbaa6f8c7c9e3ec53ad248fd77bd47
Ahmed-Zayan-Ahmed <okkaax@gmail.com>
2021-12-12 20:58:12 +0500

    If conditions

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php
-    $v1 = "5"; //string
-    $v2 = 8; //integer
-    //echo
-    echo $v1 + $v2;
-?>
+    $marks = 20;
+
+    /* if ($marks > 74.5) {
+        echo "Grade A";
+    } else if ($marks > 64.5) {
+        echo "Grade B";
+    } else if ($marks > 50) {
+        echo "Grade C";
+    } else {
+        echo "You've failed!";
+    } */
+
+    switch ($marks) {
+        case ($marks > 74.5):
+            echo "Grade A";
+            break;
+        case ($marks > 64.5):
+            echo "Grade B";
+            break;
+        case ($marks > 50):
+            echo "Grade C";
+            break;
+        Default:
+            echo "You've failed";
+            break;
+    }
