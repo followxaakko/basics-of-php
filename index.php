39e662749da6f31f06417e6548fdbacbb1ffe98e
Ahmed-Zayan-Ahmed <okkaax@gmail.com>
2021-12-12 21:20:57 +0500

    Class 7 : all

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-    $marks = 20;
 
+    $marks = 80;
+        //if conditions
     /* if ($marks > 74.5) {
         echo "Grade A";
     } else if ($marks > 64.5) {
@@ -10,8 +11,8 @@
     } else {
         echo "You've failed!";
     } */
-
-    switch ($marks) {
+        // switch statements
+    /* switch ($marks) {
         case ($marks > 74.5):
             echo "Grade A";
             break;
@@ -21,7 +22,22 @@
         case ($marks > 50):
             echo "Grade C";
             break;
+        case ($marks > 45):
+            echo "Grade D";
+            break;
         Default:
             echo "You've failed";
             break;
-    }
+    } */
+        //Ternary Operator
+    $grade = $marks > 74.5 ? 'Grade A' : "You've failed!";
+    echo $grade;
+
+            //short form
+    echo $marks ?: 'No Marks';
+            //long form
+    echo $marks ? $marks : 'No Marks';
+            //short form : Null coalescing operator
+    echo $marks ?? 'No Marks';
+            //lomg form : Null coalescing operator
+    echo isset($marks) ?: 'No Marks';
