d36dc019992b1a4c423c6220e1f4e8af77f497cd
Ahmed-Zayan-Ahmed <okkaax@gmail.com>
2021-12-13 05:53:28 +0500

    Added Variable Notes

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-
+    //variables
+/*
+        string - "How are you"
+        Integers - whole 256
+        Boolean - True / False / 1 / 0
+        Float - 25.5 7.14
+        Array - [1,2,3]
+        Null - 0 False
+*/
     $marks = 80;
         //if conditions
     /* if ($marks > 74.5) {
